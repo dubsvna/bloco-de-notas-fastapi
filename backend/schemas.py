e07eabbcbbfefafa0b10b55cd7ff669e8586a943
dubsvna <arthur.bic_@hotmail.com>
2025-09-03 17:58:06 -0300

    pydantic models base, create, update && crud operations in service

---
+++ backend/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel
+from datetime import datetime
+from typing import Optional
+
+class NotaBase(BaseModel):
+    title: str
+    description: str
+
+class NotaCreate(NotaBase):
+    pass  # Herda todos os campos como obrigatórios
+
+class Nota(NotaBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes = True ## Isso permite que o Pydantic leia dados ORM
+
+
+class NotaUpdate(NotaBase):
+    title: Optional[str] = None
+    description = Optional[str] = None
+
+    class Config:
+        extra = 'forbid' #impede campos extra
+
+
+
---
+++ backend/service.py
@@ -0,0 +1,32 @@
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+def get_notas(db:Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Nota).offset(skip).limit(limit).all()
+
+def create_notas(db:Session, nota: schemas.NotaCreate):
+    db_nota = models.Nota(
+        title=nota.title,
+        description=nota.description
+    )
+    db.add(db_nota)
+    db.commit()
+    db.refresh(db_nota)
+    return db_nota
+
+def get_nota(db: Session, id: int):
+    notas = db.query(models.Nota).filter(models.Nota.id == id)
+    return notas.first()
+
+def update_nota(db: Session, id: int, nota: schemas.NotaUpdate):
+    db_nota = get_nota(db, id)
+    db_nota.title = nota.title
+    db_nota.description = nota.description
+    db.commit()
+    db.refresh(db_nota)
+    return db_nota
+
+def delete_nota(db: Session, id: int):
+    notas = db.query(models.Nota).filter(models.Nota.id == id)
+    db.delete(notas)
+    db.commit()
