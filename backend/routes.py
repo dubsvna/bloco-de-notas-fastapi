8eec9cb6da1ef54cf25e895abd57f3e313641306
dubsvna <arthur.bic_@hotmail.com>
2025-10-19 20:04:34 -0300

    put e delete

--- backend/routes.py
+++ backend/routes.py
@@ -24,3 +24,11 @@ def get_nota(id: int, db: Session = Depends(get_db)):
 @router.post('/create_nota', response_model=schemas.NotaResponse)
 def create_nota(note: schemas.NotaCreate, db: Session = Depends(get_db)):
     return service.create_nota(db, nota=note)
+
+@router.put('/{id}', response_model=schemas.NotaResponse)
+def update_nota(note: schemas.NotaUpdate, id: int, db: Session = Depends(get_db)):
+    return service.update_nota(db, id=id, nota=note)
+
+@router.delete('/{id}', response_model=schemas.NotaResponse)
+def delete_nota(id: int, db: Session = Depends(get_db)):
+    return service.delete_nota(db, id=id)
